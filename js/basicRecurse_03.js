3cd16ad066966c2bd963927dfc900c8804c4da86
TwoFistedJustice <phaltak-1@yahoo.com>
2018-05-21 19:53:29 -0700

    Refactor - wipe out and copy code from bR_02 to start

--- js/basicRecurse_03.js
+++ js/basicRecurse_03.js
@@ -1,173 +1,129 @@
-
-/*
-Building an object
-Determine whether array or object
-  An array is looking for Elements => Element() => Value
-  An object is looking or members => Members() => Pair() => {Key() && Value(){}}
-
-
-*/
-
 var parseString = function(input) {
-  // you definitely want this instantiated to undefined. As it's a value type JSON doesn't use.
-  // var output = undefined;  // moving from top scope to inner function scope
 
-  // the char or set of chars currently under inspection
   var currentToken = '';
-
-  // the index of input currently being added to currentToken
   var currentIndex = 0;
-
-   //when a pair is found use this
-   var pairing = {
-     active: false,
-     key: undefined
-   }
-  // sets a limit to prevent infinite loops during development
-  // this would be completey removed for production
-  var limit = 80;
-
-  //RegEx Library x.match(a.thing)
-  var a = {
-    string: /"(.*)"/,
-    pair: /("(.*)":)/,
-    // key: /("(.*)")/,
+  var a = {                     //RegEx Libary x.match(a.thing)
     bool: /(?<!")(false|true)/, // will match 'true' but not '"true"'
-    knull: /(?<!")(null)/,  // because 'null' is a reserved word
+    knull: /(?<!")(null)/,
+    knumber: /[0-9]/,
+    pair: /("(.*)":)/,
+    string: /"(.*)"/,
   }
 
-  var getNextToken = function(){
-    if(limit > 0) {
-      limit--;
-      let length = input.length;
+  var arrayCase = function (){
+    let output = [];
 
-      if (length > 0) {
-        currentToken += input[currentIndex];
-        currentIndex++;
-        lexer();
-      }
-    } // end limit
-    // gets passed back and back and back
-    return output;
-  }
-
-// LEXER IS GOING AWAY
-  function lexer(){
-    // without the trim, whitespace will terminate recursion
-    currentToken = currentToken.trim();
-    if(currentToken === '{'){
-      objectCase();
-    } else if(currentToken === '}'){
-        resetToken();
-        getNextToken();   // TODO : getNextToken followed by return may cause problems
+    var parseArray = function() {
+      if(currentIndex === 0){
+        getNextToken();
+        parseArray();
+      } else if(currentIndex > 0 && currentIndex < input.length){
+        if(currentToken === ','){
+          proceed();
+        }else if(currentToken === '['){
+          resetToken();
+          getNextToken();
+          output.push(arrayCase());
+        } else if(currentToken.match(a.bool)){
+          output.push(valueCase());
+          proceed();
+        }else if(currentToken.match(a.knull)){
+          output.push(null);
+          proceed();
+        }else if(currentToken.match(a.knumber) &&
+          (input[currentIndex + 1] === ',' || input[currentIndex + 1] === ']')){
+          output.push(valueCase());
+          proceed();
+        }else {
+          getNextToken();
+          parseArray();
+        }
+      }else if(currentIndex === input.length){
         return;
-    } else if(currentToken.match(a.pair)){
-       pairCase();
-    } else if(currentToken.match(a.bool)) {
-      // booleanCase();
-    } else if(currentToken.match(a.knull)){
-      // nullCase()
-    } else if(currentToken === ','){
-      separatorCase()
+      }
+      function proceed(){
+        resetToken();
+        getNextToken();
+        parseArray();
+      }
     }
-    getNextToken();
-  }
-
-  // ****** CASES ************
-  // parses array opening and closing brace
-  // creates the array which will be returned
-  /*
-    For Monday:
-    I think that the scope of object case can do the whole prop set for the object
-    fire off getNewToken from here. Return it to here. Then go back to normal sequencing.
-
-    in objectCase:
-       Set a let  to store the key
-       Set a let  to store the value
-    Fire off getNewToken from here and
-    return it to the value let
-    Then add that whole thing as a prop
-    to output
-    Then go back to our regular programming :-)
+    parseArray();
+    return output;
+  } // END ARRAY CASE
 
 
-  */
-  function objectCase(){
-      let output = {}
-      truncateInput();
-      resetToken();
-      return output;
-  }
 
-  function pairCase(){
-// set key to undefined
-    //set bool to indicate a value is needed (set it back when done)
-    truncateInput();
-    if(currentToken.match(a.pair)){
-      let key = setKey(currentToken);
-      pairing.active = true;
-      pairing.key = key;
-      output[key] = undefined;
-      resetToken();
-      console.log('if I call getNextToken from here, would it cascade back to here? I think so....');
+  var objectCase = function(){
+    let output = {};
 
+    var parseObject = function(){
+      // do stuff with objects
+
+    };
+
+    parseObject();
+    return output;
+  } // END OBJECT CASE
+
+  /* *********** HELPERS ********************/
+  function valueCase() {
+    // console.log('valcase', currentToken);
+    if (currentToken === 'true') {
+      return true;
+    } else if (currentToken === 'false') {
+      return false;
+    }else if (currentToken === 'null'){
+      return null;
+    }else if(currentToken.match(a.knumber)){
+      return Number(currentToken);
 
     }else{
-      throw('pairCase: token is not a pair');
+      throw('valueCase() error');
     }
+
+    resetToken();
   }
-
-  // parses commas
-  // separators mean 'the string value is at an end'
-  // because they always FOLLOW the value.
-  function separatorCase(){}
-
-  // ************** HELPER FUNCTIONS ****************
 
   function resetToken(){
     currentToken = '';
   }
 
-  // call this after something is found in the input
-  var truncateInput = function(){
-    if( input.length > 0) {
-      // let old = input;
-      input = input.slice(currentIndex, input.length);
-      currentIndex = 0;
-      // console.log('input truncated from ' + old + ' to ' + input);
-    } else {
-      throw('truncateInput() error: input string has been reduced to zero length');
-    }
-  };
+  function getNextToken () {
+    currentIndex++;
+    currentToken += input[currentIndex];
+    currentToken = currentToken.trim();
+  }
 
-  return getNextToken();
-} // END ParseString()
-
-
-// it retrieves whatever is between quotes
-function setKey(str){
-  return str.replace(/"([^"]+(?="))"/g, '$1');
+  if(input[0] === '['){
+    return arrayCase();
+  } else if(input[0] === '{'){
+    return objectCase();
+  } else {
+    return undefined;
+  }
 }
 
-
 // ***************** ASSERTIONS **************************** //
-
-
 var testStrings = [
-  '{}',
-  '{"foo": "bar"}',
-  '{"a": "b", "c": "d"}',
-  '{"foo": true, "bar": false, "baz": null}',
-  '{"boolean, true": true, "boolean, false": false, "null": null }',
+  '[]',
+  '[false]',
+  '[true, false, false]',
+  '[true, false, null, false]',
+  '[true, false,[ null, false]]',
+  '[1, 3, 44, 1]',
+  '[1, 3, -1]',
+  '[1, 0, -1, -0.3, 0.3, 1343.32, 3345, 0.00011999999999999999]',
+  '["one", "two"]',
+  // '{"foo": "bar"}',
+  // '{"a": "b", "c": "d"}',
+  // '{"foo": true, "bar": false, "baz": null}',
+  // '{"boolean, true": true, "boolean, false": false, "null": null }',
+
+
 ];
 
 
-
-assertObjectsEqual(parseString(testStrings[0]), JSON.parse(testStrings[0])); //
-assertObjectsEqual(parseString(testStrings[1]), JSON.parse(testStrings[1])); //
-assertObjectsEqual(parseString(testStrings[2]), JSON.parse(testStrings[2]));
-assertObjectsEqual(parseString(testStrings[3]), JSON.parse(testStrings[3]));
-assertObjectsEqual(parseString(testStrings[4]), JSON.parse(testStrings[4]));
+testStrings.forEach(str => assertObjectsEqual(parseString(str), JSON.parse(str)));
 
 
 function assertObjectsEqual(actual, expected){
